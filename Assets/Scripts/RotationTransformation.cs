80cc87748b700f22684ad8921c68943a90c730b3
Porrith Suong <psuong95@gmail.com>
2019-02-09 11:36:09 -0500

    Fixes the angle computations in trig form.

--- Assets/Scripts/RotationTransformation.cs
+++ Assets/Scripts/RotationTransformation.cs
@@ -7,15 +7,32 @@ namespace Rendering {
         public Vector3 rotation;
 
         public override Vector3 Apply(Vector3 pt) {
+            var radX = rotation.x * Mathf.Deg2Rad;
+            var radY = rotation.y * Mathf.Deg2Rad;
             var radZ = rotation.z * Mathf.Deg2Rad;
+            var sinX = Mathf.Sin(radX);
+            var cosX = Mathf.Cos(radX);
+            var sinY = Mathf.Sin(radY);
+            var cosY = Mathf.Cos(radY);
             var sinZ = Mathf.Sin(radZ);
-            var cosZ = Mathf.Sin(radZ);
+            var cosZ = Mathf.Cos(radZ);
 
-            return new Vector3 (
-                pt.x * cosZ - pt.y * sinZ,
-                pt.x * sinZ + pt.y * cosZ,
-                pt.z
+            var xAxis = new Vector3(
+                cosY * cosZ,
+                cosX * sinZ + sinX * sinY * cosZ,
+                sinX * sinZ - cosX * sinY * cosZ
             );
+            var yAxis = new Vector3(
+                -cosY * sinZ,
+                cosX * cosZ - sinX * sinY * sinZ,
+                sinX * cosZ + cosX * sinY * sinZ
+            );
+            var zAxis = new Vector3(
+                sinY,
+                -sinX * cosY,
+                cosX * cosY
+            );
+            return xAxis * pt.x + yAxis * pt.y + zAxis * pt.z;
         }
     }
 }
